9c995e49f0472028fc94e97b41070e13f5b52e50
Mujakathali <mujaali481@gmail.com>
2023-12-14 12:40:52 +0530

    LoginSignup Page

---
+++ Project/src/App.js
@@ -0,0 +1,10 @@
+// App.js
+import React from "react";
+import LoginSignup from "./components/LoginSignup";
+import "./App.css"
+
+const App = () => {
+  return <LoginSignup />;
+};
+
+export default App;
